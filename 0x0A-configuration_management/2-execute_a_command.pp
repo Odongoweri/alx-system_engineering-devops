0414ed90d4ed5aaa0e72423250400a6dcb83c8f9
Odongoweri <margaretodongowerik@gmail.com>
2022-09-20 18:28:29 +0300

    Configuration management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# Kills a process named killmenow
+
+exec { 'pkill -f killmenow':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+[A[3~[3~[3~[3~[C[C[[A[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C
